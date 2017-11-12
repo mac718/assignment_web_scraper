f962c593d36fa8434857200a2ea52c94c03d8540
mac718 <mac718@gmail.com>
2017-11-12 11:06:12 -0500

    organized craped data in a hash

--- web_scraper.rb
+++ web_scraper.rb
@@ -2,51 +2,50 @@ require 'rubygems'
 require 'mechanize'
 require 'bundler/setup'
 require 'pry'
+require 'csv'
 
 class WebScraper
 
   def initialize
-    @scraper = Mechanize.new
+    @scraper = Mechanize.new { |agent| 
+          agent.user_agent_alias = "Linux Firefox"}
     @page = @scraper.get('https://www.dice.com/')
     @search = @page.form_with(:action => '/jobs')
     @search.q = "ruby rails"
-    @search.l = "Ann Arbor, MI"
+    @search.l = "San Francisco, CA"
     @scraper.history_added = Proc.new { sleep 0.5 }
+    @results = []
+  end
+
+  def search
+    @divs.map do |div|
+      {
+      company: div.at('a .compName').text.strip,
+      location: div.at('span .jobLoc').text,
+      title: div.at('h3 a').attributes['title'].value,
+      link: div.at('h3 a').attributes['href'].value,
+      posting_date: div.at('span[@itemprop = datePosted]').text
+      }
+    end
+
+  end
+
+  def next_page
+    @page = @scraper.submit(@search)
+
   end
 
   def scrape
-    titles = []
-    companies = []
-    locations = []
     @page = @scraper.submit(@search)
     @divs = @page.search('.complete-serp-result-div')
-
-    @page.search('.complete-serp-result-div').each do |div|
-      companies << div.at('a .compName').text.strip
-    end
-
-    @page.search('.complete-serp-result-div').each do |div|
-      locations << div.at('span .jobLoc').text
-    end
-
-    @page.search('.complete-serp-result-div').each do |div|
-      titles << div.at('h3 a').text.strip
-    end
-    
-   
-    companies
-    
-    locations
-    
-    titles
-    
+    search
   end
-
   def display_results
     pp scrape
   end
 
 end
+
 
 scrape = WebScraper.new
 scrape.display_results
